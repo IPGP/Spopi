053584cb388a5e60fdf84e742777e7286a3b3d08
bonaime <bonaime@ipgp.fr>
2016-03-09 15:53:21 +0100

    first commit

---
+++ install.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+# Usefull packages installation
+sudo apt-get install -y python-lxml python-scipy python-sqlalchemy python-suds python-tornado python-future subversion unclutter subversion xdotool dnsutils imagemagick libblas-dev liblapack-dev libatlas-base-dev gfortran libxslt1-dev tk-dev pyton-matplotlib x11vnc
+
+# Remove  unused packages
+sudo apt-get --purge remove   epiphany-.* scratch* sonic-pi libreoffice* claws-mail greenfoot bluej alacarte  wolfram-engine omxplayer python-picamera wpasupplicant
+sudo apt-get autoremove
+
+
+#updates
+sudo rpi-update
+sudo apt-get update
+sudo apt-get upgrade
+
+# Obspy installation
+sudo  echo  'deb http://deb.obspy.org jessie main' >> /etc/apt/sources.list
+sudo wget --quiet -O - https://raw.github.com/obspy/obspy/master/misc/debian/public.key | sudo apt-key add -
+
+
+sudo apt-get update
+sudo  apt-get upgrade
+sudo apt-get install python-obspy
+pip install lxml sqlalchemy
+
+
+sudo pip install https://github.com/bonaime/seedlink_plotter/archive/master.zip
