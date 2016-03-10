0211bf11375b977c146723aaa3e92321e892714d
Sebastien <bonaime@ipgp.fr>
2016-03-10 10:33:26 +0100

    Update spopi_install.bash

--- spopi_install.bash
+++ spopi_install.bash
@@ -46,5 +46,17 @@ sudo echo '@lxpanel --profile LXDE-pi
 sed -i 's/BLANK_TIME=30/BLANK_TIME=0/'  /etc/kbd/config
 sed -i 's/^POWERDOWN_TIME*/POWERDOWN_TIME=0/'  /etc/kbd/config
 
+#Spopi files
+mkdir /home/pi/visu
+wget -O /home/pi/visu/drum-line  https://raw.githubusercontent.com/IPGP/Spopi/master/visu/drum-line
+wget -O /home/pi/visu/screen_off.bash https://raw.githubusercontent.com/IPGP/Spopi/master/visu/screen_off.bash
+wget -O /home/pi/visu/screen_on.bash https://raw.githubusercontent.com/IPGP/Spopi/master/visu/screen_on.bash
+wget -O /home/pi/visu/two-drums https://raw.githubusercontent.com/IPGP/Spopi/master/visu/two-drums
 
+#Crontabs
+(crontab -l ; echo "@reboot /usr/bin/unclutter -display :0 &") | crontab -
+(crontab -l ; echo "# Spopi On") | crontab -
+(crontab -l ; echo "00 04  * * *  DISPLAY=:0 /home/pi/visu/screen_on.bash  2>/tmp/cron_stderr 1>/tmp/cron_stdout") | crontab -
+(crontab -l ; echo "# Spopi Off") | crontab -
+(crontab -l ; echo "00 14  * * * /home/pi/visu/screen_off.bash") | crontab -
 
